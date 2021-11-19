1047c21ef2c7328ffb7d20af5eed371594a81df0
Braks <78412429+bcakmakoglu@users.noreply.github.com>
2021-11-19 16:29:24 +0100

    fix: gridpath undefined

--- src/edge/PathFindingEdge.vue
+++ src/edge/PathFindingEdge.vue
@@ -11,7 +11,7 @@ import {
   EdgeProps,
   useVueFlow,
 } from '@braks/vue-flow'
-import { createGrid, PointInfo, gridRatio } from './createGrid'
+import { createGrid, gridRatio } from './createGrid'
 import { drawSmoothLinePath } from './drawSvgPath'
 import { generatePath } from './generatePath'
 import { getBoundingBoxes } from './getBoundingBoxes'
@@ -62,55 +62,61 @@ const props = withDefaults(defineProps<PathFindingEdgeProps>(), {
 })
 
 const store = useVueFlow()
-// We use the node's information to generate bounding boxes for them
-// and the graph
-const bb = computed(() => getBoundingBoxes(store.nodes, nodePadding, graphPadding, roundCoordinatesTo))
-const source = computed<PointInfo>(() => ({
-  x: props.sourceX,
-  y: props.sourceY,
-  position: props.sourcePosition,
-}))
-const target = computed<PointInfo>(() => ({
-  x: props.targetX,
-  y: props.targetY,
-  position: props.targetPosition,
-}))
-
-// We then can use the grid representation to do pathfinding
-const gridPath = computed(() => {
-  if (source.value.x && target.value.x && bb.value.nodes && bb.value.graph) {
-    // With this information, we can create a 2D grid representation of
-    // our graph, that tells us where in the graph there is a "free" space or not
-    const g = createGrid(bb.value.graph, bb.value.nodes, source.value, target.value)
-    return generatePath(g.grid, g.start, g.end)
-  } else {
-    return []
-  }
-})
-
-// Here we convert the grid path to a sequence of graph coordinates.
-const graphPath = computed(() =>
-  gridPath.value?.map((gridPoint) => {
-    const [x, y] = gridPoint
-    const graphPoint = gridToGraphPoint({ x, y }, bb.value.graph.xMin, bb.value.graph.yMin)
-    return [graphPoint.x, graphPoint.y]
-  }),
-)
-
-// Finally, we can use the graph path to draw the edge
-const svgPathString = computed(() => drawSmoothLinePath(source.value, target.value, graphPath.value))
 const markerEnd = computed(() => getMarkerEnd(props.arrowHeadType, props.markerEndId))
 const centered = computed(() =>
   getEdgeCenter({
     ...props,
   }),
 )
+
+const path = computed(() => {
+  // We use the node's information to generate bounding boxes for them
+  // and the graph
+  const bb = getBoundingBoxes(store.nodes, nodePadding, graphPadding, roundCoordinatesTo)
+  const source = {
+    x: props.sourceX,
+    y: props.sourceY,
+    position: props.sourcePosition,
+  }
+  const target = {
+    x: props.targetX,
+    y: props.targetY,
+    position: props.targetPosition,
+  }
+
+  // We then can use the grid representation to do pathfinding
+  let gridPath: any[] = []
+  // With this information, we can create a 2D grid representation of
+  // our graph, that tells us where in the graph there is a "free" space or not
+  const g = createGrid(bb.graph, bb.nodes, source, target)
+  gridPath = generatePath(g.grid, g.start, g.end)
+
+  // Here we convert the grid path to a sequence of graph coordinates.
+  const graphPath = gridPath?.map((gridPoint) => {
+    const [x, y] = gridPoint
+    const graphPoint = gridToGraphPoint({ x, y }, bb.graph.xMin, bb.graph.yMin)
+    return [graphPoint.x, graphPoint.y]
+  })
+
+  // Finally, we can use the graph path to draw the edge
+  const svgPathString = drawSmoothLinePath(source, target, graphPath)
+
+  return {
+    gridPath,
+    svgPathString,
+  }
+})
 const attrs = useAttrs()
 </script>
 <template>
-  <BezierEdge v-if="gridPath.length <= 2" v-bind="{ ...props, ...attrs }" />
+  <BezierEdge v-if="path.gridPath.length <= 2" v-bind="{ ...props, ...attrs }" />
   <template v-else>
-    <path :style="{ ...props.style, ...attrs.style }" class="vue-flow__edge-path" :d="svgPathString" :marker-end="markerEnd" />
+    <path
+      :style="{ ...props.style, ...attrs.style }"
+      class="vue-flow__edge-path"
+      :d="path.svgPathString"
+      :marker-end="markerEnd"
+    />
     <EdgeText
       v-if="props.label"
       :x="centered[0]"
